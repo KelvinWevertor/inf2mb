7889fb715aa54b90a68587ac738e039b3634bf57
17259208 <17259208@DSN1271058237.cfp127aluno.local>
2018-04-04 11:30:52 -0300

    Final 04-04

---
+++ Agenda/src/br/senai/jandira/app/Agenda.java
@@ -0,0 +1,13 @@
+package br.senai.jandira.app;
+
+import br.senai.sp.jandira.dbUtils.Conexao;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		
+		Conexao.abrirConexao();
+
+	}
+
+}
